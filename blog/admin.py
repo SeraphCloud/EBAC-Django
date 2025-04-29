695343b329e05d44955924ed99c44d240cedd1f3
Victor <alexandre.victor2641@gmail.com>
2025-04-29 18:00:31 -0300

    added the administrative classes for the user and posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post
+
+# Criando a classe para administrar os posts
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created_on', 'status')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)  # Modelo e Admin class como argumentos
